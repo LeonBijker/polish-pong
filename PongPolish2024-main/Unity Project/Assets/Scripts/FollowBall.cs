b5a2baa8b1d303f9473bcb84792aacf5c73a6066
LeonBijker <0351849@student.rocvantwente.nl>
2024-09-06 09:28:39 +0200

    finished product
    het script zorgt ervoor dat het de ball volgt

---
+++ PongPolish2024-main/Unity Project/Assets/Scripts/FollowBall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cinemachine : MonoBehaviour
+{
+    Camera Camera;
+    GameObject Ball;
+    Vector3 offset = new Vector3 (0,0,-10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = FindFirstObjectByType<Camera>();
+        Ball = FindFirstObjectByType<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Camera.transform.position = Ball.transform.position + offset;
+    }
+   
+}
